bf390621a64b54a460beae0bc793ae7b1d56137a
lua-p1 <luapitra@gmail.com>
2025-10-01 17:16:22 -0300

    ok

--- Assets/Scripts/SpikeTap/WayPointsManager.cs
+++ Assets/Scripts/SpikeTap/WayPointsManager.cs
@@ -1,46 +1,52 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 public class WayPointsManager : MonoBehaviour
 {
     [SerializeField]private List<Transform> wayPoints = new List<Transform>();
     [SerializeField]private int _wayPointIndex;
-    [SerializeField]private bool _isMoving;
-    [SerializeField]private float _speed;
-    [SerializeField]private float _rotSpeed;
+    [SerializeField]private bool _isMoving = false;
     [SerializeField]private bool _isLooping;
     [SerializeField]private bool _isRandom;
+    [SerializeField]private float _minDistanceToWaypoint = 0.5f;
+    private NavMeshAgent _agent;
     private void Start()
     {
-        Patrol();
-    }
-    private void Patrol()
-    {
-        _wayPointIndex = 0;
-        //_isMoving = true;
+        _agent = GetComponent<NavMeshAgent>();
+        if (wayPoints.Count > 0)
+        {
+            _wayPointIndex = 0;
+            SetNextDestination();
+        }
     }
     private void Update()
     {
-        if (!_isMoving)return;
-        if (_wayPointIndex < wayPoints.Count)
+        if (!_isMoving || wayPoints.Count == 0) return;
+        if (!_agent.pathPending && _agent.remainingDistance <= _minDistanceToWaypoint)
         {
-            transform.position = Vector3.MoveTowards(transform.position, wayPoints[_wayPointIndex].position, Time.deltaTime * _speed);
-            var _direction = transform.position - wayPoints[_wayPointIndex].position;
-            var _targetRot = Quaternion.LookRotation(_direction, Vector3.up);
-            transform.rotation = Quaternion.Lerp(transform.rotation, _targetRot, _rotSpeed * Time.deltaTime);
-            var _distance = Vector3.Distance(transform.position, wayPoints[_wayPointIndex].position);
-            if (_distance <= 0.05f)
-
-                if(_isRandom){
-                    _wayPointIndex = Random.Range(0, wayPoints.Count);
-                }
-                else
-                {
-                    _wayPointIndex++;
-                    if (_isLooping && _wayPointIndex >= wayPoints.Count)
-                    {
-                        _wayPointIndex = 0;
-                    }
-                }               
+            if (_isRandom)
+            {
+                _wayPointIndex = Random.Range(0, wayPoints.Count);
             }
+            else
+            {
+                _wayPointIndex++;
+                if (_isLooping && _wayPointIndex >= wayPoints.Count)
+                {
+                    _wayPointIndex = 0;
+                }
+                else if (_wayPointIndex >= wayPoints.Count)
+                {
+                    _isMoving = false;
+                    return;
+                }
+            }
+            SetNextDestination();
         }
+    }
+    private void SetNextDestination()
+    {
+        if (wayPoints.Count == 0) return;
+        _agent.SetDestination(wayPoints[_wayPointIndex].position);
+    }
 }
