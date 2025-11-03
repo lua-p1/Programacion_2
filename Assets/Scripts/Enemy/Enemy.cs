f3b8b197b3093fd883ca2dd03a83b088d3319779
lua-p1 <luapitra@gmail.com>
2025-11-03 20:37:18 -0300

    okCamera

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -52,7 +52,7 @@ public class Enemy : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, _visionRange);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, GameManager.instance.player.transform.position);
+        //Gizmos.color = Color.blue;
+        //Gizmos.DrawLine(transform.position, GameManager.instance.player.transform.position);
     }
 }
