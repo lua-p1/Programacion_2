da44c750ca31237ad3cfd5fd4b156e79cd56218e
lua-p1 <luapitra@gmail.com>
2025-12-04 23:48:01 -0300

    trySliders

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -7,16 +7,23 @@ public class VolumeSettings : MonoBehaviour
     public AudioMixer audioMixer;
 
     [Header("Sliders")]
+    public Slider masterSlider;
     public Slider musicSlider;
     public Slider sfxSlider;
 
     private void Start()
     {
-        musicSlider.value = PlayerPrefs.GetFloat("Music", 0.75f);
-        sfxSlider.value = PlayerPrefs.GetFloat("Sound", 0.75f);
+        masterSlider.value = PlayerPrefs.GetFloat("Master", 1f);
+        musicSlider.value = PlayerPrefs.GetFloat("Music", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("Sound", 1f);
 
         SetMusicVolume(musicSlider.value);
         SetSFXVolume(sfxSlider.value);
+    }
+    public void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("Master", value);
     }
     public void SetMusicVolume(float value)
     {
